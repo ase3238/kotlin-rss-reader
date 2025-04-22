6bf01ee85814d0d05869ad89c769913d95af8b54
ase3238 <ase3238@gmail.com>
2025-04-22 11:39:46 +0900

    chore: rss 소스 추가

--- src/main/kotlin/rss/entity/RssSource.kt
+++ src/main/kotlin/rss/entity/RssSource.kt
@@ -5,4 +5,7 @@ val RSS_SOURCE =
         "https://aws.amazon.com/ko/blogs/tech/feed/",
         "https://techblog.woowahan.com/feed/",
         "https://developers.hyundaimotorgroup.com/blog/rss",
+        "https://helloworld.kurly.com/feed.xml",
+        "https://engineering.linecorp.com/ko/feed/index.html",
+        "https://toss.tech/rss.xml",
     )
